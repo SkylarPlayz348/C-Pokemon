8d0c3d4449b09a3be93e3aeb3924ab18561214b9
Sky <skylarplayz348@gmail.com>
2024-01-03 16:29:29 +0000

    optimize code a bit

--- console.h
+++ console.h
@@ -5,26 +5,31 @@
 /**********************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+// Uncomment if codingon macOS
+//#include <TargetConditionals.h>
 
 #ifdef __APPLE__
-    void clear(){
-        system("clear");
-    }
+void clear()
+{
+    system("clear");
+}
 #elif __unix__
-    void clear(){
-        system("clear");
-    }
+void clear()
+{
+    system("clear");
+}
 #elif WIN32
-    void clear() {
-        system("cls");
-    }
+void clear()
+{
+    system("cls");
+}
 #endif
 
 // Command storage
-char enteredCMD[10]; // Main console commands
-int battlecmd; // Battle console commands
-char pokecenterCMD[1];  // Pokecenter console commands
-char marketCMD[2]; // Market console commands
+char enteredCMD[10];   // Main console commands
+int battlecmd;         // Battle console commands
+char pokecenterCMD[1]; // Pokecenter console commands
+char marketCMD[2];     // Market console commands
 
 // Console Variables
 int consoleRet = 1;
@@ -42,14 +47,20 @@ int pPN; // Player Pokemon Number
 int oPN; // Opponent Pokemon Number
 
 // Pokecenter Sub-Console
-int pokeConsole (char command[]){
-    if (strcmp(command, "y") == 0) {
+int pokeConsole(char command[])
+{
+    if (strcmp(command, "y") == 0)
+    {
         printf("Ok just hand over your pokemon\nYou left the PokeCenter");
         consoleRet = 7;
-    } else if (strcmp(command, "n") == 0) {
+    }
+    else if (strcmp(command, "n") == 0)
+    {
         printf("Ok come back another time\n");
         consoleRet = 8;
-    } else {
+    }
+    else
+    {
         clear();
         printf("Enter A Valid Command!\n");
         consoleRet = 1;
@@ -57,33 +68,48 @@ int pokeConsole (char command[]){
     return consoleRet;
 }
 
-int martConsole (char command[]){
-    if (strcmp(command, "buy") == 0){
-
-    } else if (strcmp(command, "sell") == 0){
-
-    } else if (strcmp(command, "exit") == 0){
-
-    } else {
-        printf("Enter A Valid Command!\n")
+int martConsole(char command[])
+{
+    if (strcmp(command, "buy") == 0)
+    {
+    }
+    else if (strcmp(command, "sell") == 0)
+    {
+    }
+    else if (strcmp(command, "exit") == 0)
+    {
+    }
+    else
+    {
+        printf("Enter A Valid Command!\n");
     }
 }
 
-int console (char command[]){
-    if(strcmp(command, "battle") == 0){
+int console(char command[])
+{
+    if (strcmp(command, "battle") == 0)
+    {
         clear();
         printf("You entered a battle\n");
         consoleRet = 1;
-    } else if (strcmp(command, "pokecenter") == 0) {
+    }
+    else if (strcmp(command, "pokecenter") == 0)
+    {
         clear();
-        printf("Would you like your pokemon healed(y/n)\n");
+        printf("Would you like your pokemon healed\n(y/n):");
         consoleRet = 6;
-    } else if (strcmp(command, "help") == 0){
+    }
+    else if (strcmp(command, "help") == 0)
+    {
         printf("\n-----Commands-----\nbattle - Enter a battle(Only works on Routes).\npokecenter - Enter the Pokecenter(Only works if your in a city).\npokemart - Enter the Pokemart(Only works in a City).\nhelp - this command.\n\n");
         consoleRet = 1;
-    } else if (strcmp(command, "exit") == 0) {
+    }
+    else if (strcmp(command, "exit") == 0)
+    {
         consoleRet = 0;
-    } else {
+    }
+    else
+    {
         clear();
         printf("Enter A Valid Command! Type the command help to get all available commands\n");
         consoleRet = 1;
--- main.c
+++ main.c
@@ -3,41 +3,34 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include "console.h"
 
-#ifdef __APPLE__ 
-    #include "console-mac.h"
-#else
-    #include "console.h"
-#endif
-
-int start() {
+int start()
+{
     clear();
     while (consoleRet != 0)
-    {   
+    {
         switch (consoleRet)
         {
-            case 6:
-                printf("Enter Command: ");
-                scanf("%s", enteredCMD);
-                pokeConsole(enteredCMD);
-                break;
-            case 7:
-                printf("Your Pokemon are Healed\n");
-                consoleRet = 8;
-                break;
-            default:
-                printf("Enter Command: ");
-                scanf("%s", enteredCMD);
-                console(enteredCMD);
-                break;
+        case 6:
+            scanf("%s", enteredCMD);
+            pokeConsole(enteredCMD);
+            break;
+        case 7:
+            printf("Your Pokemon are Healed\n");
+            consoleRet = 8;
+            break;
+        default:
+            printf("Enter Command: ");
+            scanf("%s", enteredCMD);
+            console(enteredCMD);
+            break;
         }
     }
-    return 0;
+    return consoleRet;
 }
 
-int main() {
-    while(start() != 0){
-        start();
-    }
-    return 0;
+int main()
+{
+    return start();
 }
